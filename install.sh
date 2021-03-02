50c419d0d1b738685440dc3133ed6ea348dac861
Prescott Murphy <prurph@gmail.com>
2021-03-02 12:58:01 +0000

    Scaffold zsh config

--- install.sh
+++ install.sh
@@ -13,3 +13,9 @@ ln -s "$HOME/dotfiles/X11" "$HOME/.config"
 # *** i3
 rm -rf "$HOME/.config/i3"
 ln -s "$HOME/dotfiles/i3" "$HOME/.config"
+
+# *** zsh
+mkdir -p "$HOME/.config/zsh"
+ln -sf "$HOME/dotfiles/zsh/.zshenv" "$HOME"
+# zshenv will have set up XDG_CONFIG_HOME to .config
+ln -sf "$HOME/dotfiles/zsh/.zshrc" "$HOME/.config/zsh"
